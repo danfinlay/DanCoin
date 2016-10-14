404195de1752e91afa406ca4339f14c3d3788cb3
Dan Finlay <dan@danfinlay.com>
2016-10-13 19:31:25 -0700

    Add minting limit

--- contracts/DanCoin.sol
+++ contracts/DanCoin.sol
@@ -5,15 +5,35 @@ import "Owned.sol";
 
 
 contract DanCoin is HumanStandardToken, Owned {
-  function DanCoin() {
+  uint lastMinted;
+  uint oneWeek = 604800;
+
+  function DanCoin(
+    uint256 _initialAmount,
+    string _tokenName,
+    uint8 _decimalUnits,
+    string _tokenSymbol
+  ) {
+
+    balances[msg.sender] = _initialAmount;       // Give the creator all initial tokens
+    totalSupply = _initialAmount;                // Update total supply
+    name = _tokenName;                           // Set the name for display purposes
+    decimals = _decimalUnits;                    // Amount of decimals for display purposes
+    symbol = _tokenSymbol;                       // Set the symbol for display purposes
+    owner = msg.sender;                          // Assign creator as Dan, the owner.
+    lastMinted = block.timestamp;                // Set the minted time to the current date.
   }
 
   // Once a week, Dan can mint up to 160 tokens (40 hours worth).
   function mintToken(uint256 mintedAmount) onlyOwner {
     if (mintedAmount > 160) throw;
+    if (lastMinted + oneWeek > block.timestamp) throw;
+
     balanceOf[owner] += mintedAmount;
     totalSupply += mintedAmount;
     Transfer(0, owner, mintedAmount);
+
+    lastMinted = block.timestamp;
   }
 
   // In the most extreme cases, Dan can burn anyone's tokens.
